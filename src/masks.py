db00c674181d75bacf253fb7e8b74102bbddba8b
Aleksandra Volchikhina <aleksandravolcihina@gmail.com>
2024-08-04 22:16:03 +0300

    commit for tests

--- src/masks.py
+++ src/masks.py
@@ -4,25 +4,31 @@ def get_mask_card_number(card_number: str) -> str:
     number_part1 = ""
     number_part2 = ""
     number_part3 = ""
+    card_number_new = card_number.replace(" ", "")
 
-    for el in card_number[:4]:
-        number_part1 += el
-    new_card_number += number_part1 + " "
-    for el in card_number[4:6]:
-        number_part2 += el
-    new_card_number += number_part2 + "**" + " " + "****" + " "
-    for el in card_number[12:]:
-        number_part3 += el
-    new_card_number += number_part3
-    return new_card_number
+    if 20>=len(card_number_new) >= 16 and card_number_new.isdigit:
+        for el in card_number_new[:4]:
+            number_part1 += el
+        new_card_number += number_part1 + " "
+        for el in card_number_new[4:-10]:
+            number_part2 += el
+        new_card_number += number_part2 + "**" + " " + "****" + " "
+        for el in card_number_new[-4:]:
+            number_part3 += el
+        new_card_number += number_part3
+        return new_card_number
+    else:
+        return "Введите корректный номер карты"
 
 
 def get_mask_account(card_number: str) -> str:
     """Функцию маскировки номера банковского счета"""
     new_card_number = ""
     number_part = ""
-
-    for el in card_number[12:]:
-        number_part += el
-    new_card_number = "**" + number_part
-    return new_card_number
+    if 17 <= len(card_number) <= 25:
+        for el in card_number[-4:]:
+            number_part += el
+        new_card_number = "**" + number_part
+        return new_card_number
+    else:
+        print("Введите номер счета корректной длины")
--- src/processing.py
+++ src/processing.py
@@ -1,8 +1,10 @@
-def filter_by_state(dictionary: list[dict[str]], state="EXECUTED") -> list[dict[str]]:
+def filter_by_state(dictionary = [], state="EXECUTED") -> [dict]:
     """Функция формирования нового списка словарей по ключевому слову"""
     new_dictionary = []
     for el in dictionary:
-        if el.get("state") == state:
+        if state == None or state == "" or state == " ":
+            print("Введите корректное state")
+        elif el.get("state") == state:
             new_dictionary.append(el)
 
     return new_dictionary
@@ -10,5 +12,5 @@ def filter_by_state(dictionary: list[dict[str]], state="EXECUTED") -> list[dict[
 
 def sort_by_date(dictionary: list[dict[str]], a=True) -> list[dict[str]]:
     """Функция формирования нового списка, отсортированных по дате"""
-    sorted_dict = sorted(dictionary, key=lambda d: d["date"], reverse=a)
+    sorted_dict = sorted(dictionary, key=lambda d: (d["date"].year, d["date"].month, d["date"].day), reverse=a)
     return sorted_dict
